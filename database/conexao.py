857c8cc4a13800a828c3240b42d9bbdcc62ab80b
Helena Rosa <helena.rosa@aluno.senai.br>
2026-03-27 16:13:07 -0300

    mudanças

--- database/conexao.py
+++ database/conexao.py
@@ -3,13 +3,29 @@ import mysql.connector
 
 
 def conectar ():
-    conexao = mysql.connector.connect(
-        host = "localhost",
-        port = 3306,
-        user = "root",
-        password = "root",
-        database = "lenamusic"
+    tipo_local = "nuvem"
+    if tipo_local == "local":
+
+        conexao = mysql.connector.connect(
+            host = "localhost",
+            port = 3306,
+            user = "root",
+            password = "root",
+            database = "lenamusic"
     )
+
+        cursor = conexao.cursor(dictionary=True)
+    else:
+        conexao = mysql.connector.connect(
+        host = "mysql-2b19cdd4-helenarosa-servidor.a.aivencloud.com",
+        port = 12587,
+        user = "avnadmin",
+        password = "AVNS_QUnXnjkva_KcezN4XSF",
+        database = "defaultdb"
+    )
+       
+    
+    
 
 
     #CRIANDO CURSOR
